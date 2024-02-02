3c1c69edd56f656aba831898fbb313d876286a51
Chandler Mayberry <44760635+mainmanchandler@users.noreply.github.com>
2024-02-02 12:49:38 -0500

    Jquery practice

--- 20.0 Simon Game/game.js
+++ 20.0 Simon Game/game.js
@@ -2,7 +2,15 @@ var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 var userPattern = [];
 
+var begin = false;
+var level = 0; //lvl counter
+
 function nextSequence(){
+    userPattern = [] //to reset on every iteration
+
+    level++;
+    $("#level-title").text("Level " + level);
+
     var randomNumber = Math.round(Math.random() * (4 - 1) + 0);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
@@ -16,6 +24,7 @@ $('.btn').click(function(){
     userPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
+    checkAnswer(userPattern.length-1);
 });
 
 function playSound(color){
@@ -30,4 +39,45 @@ function animatePress(currentColour){
         $("#" + currentColour).removeClass("pressed");
     }, 100);
 }
+
+$(document).keypress(function(){
+    if (!begin){
+        $("#level-title").text("Level" + level);
+        nextSequence();
+        started = true;
+    }   
+});
+
+function checkAnswer(currentLevel){
+    if (gamePattern[currentLevel] === userPattern[currentLevel]){
+        console.log("user answer correct");
+        if (userPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        console.log("user answer incorrect");
+        playSound("wrong")
+
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("#level-title").text("Game Over, Press any key to restart");
+        gameReset();
+    }
+}
+
+function gameReset(){
+    level = 0;
+    gamePattern = [];
+    begin = false;
+}
+
+
+
+
+
 
